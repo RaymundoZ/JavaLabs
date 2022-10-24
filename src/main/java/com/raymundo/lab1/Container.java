327e5df83b14ac9fe095029385dc6a377eae389c
RaymundoZ <andrej.dron2002@gmail.com>
2022-10-24 18:30:34 +0300

    Added docs

--- src/main/java/com/raymundo/lab1/Container.java
+++ src/main/java/com/raymundo/lab1/Container.java
@@ -1,55 +1,148 @@
 package com.raymundo.lab1;
 
+/**
+ * A container class that stores an arbitrary number of objects
+ *
+ * @param <T> Stored object type
+ * @author RaymundoZ
+ */
 public class Container<T> {
 
+    /**
+     * @value Default value of the array size when using the constructor with no parameters
+     */
     private static final int INIT_SIZE = 10;
+
+    /**
+     * @value Array of objects
+     */
     private Object[] array;
+
+    /**
+     * @value The current position of the index in the array
+     */
     private int index;
+
+    /**
+     * @value The current array size
+     */
     private int size;
 
+    /**
+     * A constructor with no parameters that initializes an array with a default value {@link Container#INIT_SIZE}
+     */
     public Container() {
         this.array = new Object[INIT_SIZE];
         this.index = 0;
         this.size = INIT_SIZE;
     }
 
+    /**
+     * A constructor that initializes an array with parameter <b>size</b>
+     *
+     * @param size Size of an array
+     */
     public Container(int size) {
         this.array = new Object[size];
         this.index = 0;
         this.size = size;
     }
 
+    /**
+     * A method that adds an object to the end of an array
+     *
+     * @param object Object to insert
+     */
     public void add(T object) {
         if (isArrayFull())
             expand();
         array[index++] = object;
     }
 
+    /**
+     * A method that adds an object to an array by shifting elements and inserting that element to free space
+     *
+     * @param object Object to insert
+     * @param index  Index where to insert object
+     */
     public void add(T object, int index) {
+        checkIndex(index);
         if (isArrayFull())
             expand();
         shiftUp(index);
         array[index] = object;
     }
 
+    /**
+     * A method that removes an object by index from an array by shifting elements
+     *
+     * @param index Index of object to remove
+     */
     public void remove(int index) {
+        checkIndex(index);
         shiftDown(index);
     }
 
+    /**
+     * A method that removes an object from an array by shifting elements
+     *
+     * @param object Object to remove
+     */
     public void remove(T object) {
         for (int i = 0; i < size; i++)
             if (object.equals(array[i]))
                 remove(i);
     }
 
+    /**
+     * A method that returns an object by index
+     *
+     * @param index Index of object to return
+     * @return Object of type <b>T</b> by input parameter index
+     */
     public T get(int index) {
+        checkIndex(index);
         return (T) array[index];
     }
 
+    /**
+     * A method that rewrites an object in array by index
+     *
+     * @param object Object to rewrite
+     * @param index  Index of object to rewrite
+     */
+    public void set(T object, int index) {
+        checkIndex(index);
+        array[index] = object;
+    }
+
+    /**
+     * A method that returns the index of the passed object
+     *
+     * @param object Object to find index
+     * @return <b>int</b> index
+     */
+    public int getIndexOf(T object) {
+        for (int i = 0; i < size; i++)
+            if (object.equals(array[i]))
+                return i;
+        return -1;
+    }
+
+    /**
+     * A method that returns the current size of an array
+     *
+     * @return <b>int</b> size
+     */
     public int getSize() {
         return size;
     }
 
+    /**
+     * A method that overrides method {@link Object#toString()} to represent a Container object as string
+     *
+     * @return <b>String</b> container
+     */
     @Override
     public String toString() {
         String str = "[";
@@ -59,21 +152,44 @@ public class Container<T> {
         return str.substring(0, str.length() - 2).concat("]");
     }
 
+    /**
+     * A method that checks if the passed index is correct. Otherwise throws {@link IncorrectIndexException}
+     *
+     * @param index Index to check
+     */
+    private void checkIndex(int index) {
+        if (index >= size || index < 0)
+            try {
+                throw new IncorrectIndexException("There is no such an index in array");
+            } catch (IncorrectIndexException e) {
+                throw new RuntimeException(e);
+            }
+    }
+
+    /**
+     * A method that checks if all array cells are filled
+     *
+     * @return <b>boolean</b> if array is full
+     */
     private boolean isArrayFull() {
         return index >= size;
     }
 
-    private void expand(int amount) {
-        size += amount;
-        Object[] newArray = new Object[size];
+    /**
+     * A method that expands an array by 1 when it is full
+     */
+    private void expand() {
+        Object[] newArray = new Object[++size];
         copy(array, newArray);
         array = newArray;
     }
 
-    private void expand() {
-        expand(10);
-    }
-
+    /**
+     * A method that copies array <b>a</b> to array <b>b</b>. The size of an array <b>a</b> must not exceed that of array <b>b</b>
+     *
+     * @param a Array to copy
+     * @param b Array where to copy
+     */
     private void copy(Object[] a, Object[] b) {
         if (b.length >= a.length) {
             for (int i = 0; i < a.length; i++)
@@ -81,19 +197,29 @@ public class Container<T> {
         }
     }
 
+    /**
+     * A method that shifts array elements forward by 1 from passed index
+     *
+     * @param index Index from which to shift
+     */
     private void shiftUp(int index) {
         Object[] arr = new Object[size];
         int counter = 0;
         for (int i = index; i < size; i++)
             arr[counter++] = array[i];
         counter = 0;
-        expand(1);
+        expand();
         for (int i = index + 1; i < size; i++)
             array[i] = arr[counter++];
         array[index] = null;
         this.index++;
     }
 
+    /**
+     * A method that shifts array elements back by 1 from passed index
+     *
+     * @param index Index from which to shift
+     */
     private void shiftDown(int index) {
         Object[] arr = new Object[size];
         int counter = 0;
